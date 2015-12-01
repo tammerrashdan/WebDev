1cf2de8e8a2cbb0cdccb581df2022362f74b9ff6
tammerrashdan <Tammer@Tammers-MacBook-Air.local>
2015-12-01 18:12:55 -0500

    bunch of stuff

---
+++ Final Project/Main/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$("a.grouped1").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false
+	});
+	
+});
